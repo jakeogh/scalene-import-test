20f2667c8fa7c4ea73117b657b8fab2cba4310c6
jakeogh <github.com@v6y.net>
2022-10-20 21:22:28 -0700

    test if a generic `class Bla(ValueError)` triggers it (it does not)

--- scalene_import_test/scalene_import_test.py
+++ scalene_import_test/scalene_import_test.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
+# this triggers the error
 from greendb import CommandError
+
+# this does not https://github.com/jakeogh/test-single-exception-class
+# from test_single_exception_class import SomeValueError
+
 
 def cli() -> None:
     print("here")
